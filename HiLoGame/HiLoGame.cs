4d61a89c39805bcdc79ce1babdc112451d341812
Erlend Bergqvist Andersen <237196@usn.no>
2022-03-04 14:51:03 +0100

    Add project files.

---
+++ HiLoGame/HiLoGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiLoGame
+{
+    internal static class HiLoGame
+    {
+        public const int MAXIMUM = 10;
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);
+        private static int nextNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 10;
+
+        public static int GetPot()
+        {
+            return pot;
+        }
+
+        public static void Guess(bool higher)
+        {
+            if((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+
+                pot++;
+            }
+            else
+            {
+                Console.WriteLine("Bad luck, you guessed wrong.");
+
+                pot--;
+            }
+
+            currentNumber = nextNumber;
+
+            nextNumber = random.Next(1, MAXIMUM + 1);
+
+            Console.WriteLine("The current number is " + currentNumber);
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+
+            if(nextNumber >= half)
+            {
+                Console.WriteLine("The next number is at least " + half);
+            }
+            else
+            {
+                Console.WriteLine("The next number is at most " + half);
+            }
+
+            pot--;
+        }
+    }
+}
